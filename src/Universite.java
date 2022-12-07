16ed138d1d7ca62107bfaca245963c07d17af695
Your Name <ouissalbraik02@gmail.com>
2022-12-07 12:49:13 +0000

    affichage class

--- src/Universite.java
+++ src/Universite.java
@@ -11,7 +11,19 @@ public class Universite {
 	this.nom = nom;
 	this.pack = pack;
 }
+public class affichag  {
 
+	private int i_univ; //identifiant unique de l'universit�
+	private String nom;
+	private TypePackage pack;
+	
+	   public affichag(int i_univ, String nom, TypePackage pack) {
+	   super();
+	   this.i_univ = i_univ;
+	   this.nom = nom;
+	   this.pack = pack;
+   }
+   
 	public void setPack(TypePackage pack) {
 		this.pack = pack;
 	}
