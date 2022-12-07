a0f9704e61db5d1dc09a84f973f4f5588d6d2ab7
Your Name <ouissalbraik02@gmail.com>
2022-12-07 12:12:53 +0000

    try

--- src/EtudiantRepository.java
+++ src/EtudiantRepository.java
@@ -18,12 +18,27 @@ public class EtudiantRepository {
 		int rs = stmt.executeUpdate(sql);
 		
 		if (rs == 1){
-				System.out.println("log : ajout dans la BD réussi de l'étudiant  du Matricule" + E.getMatricule());
+				System.out.println("log : ajout dans la BD rï¿½ussi de l'ï¿½tudiant  du Matricule" + E.getMatricule());
 			}else if (rs == 0){
-				System.out.println("log : Echec de l'ajout dans la BD de l'étudiant  du Matricule" + E.getMatricule());
+				System.out.println("log : Echec de l'ajout dans la BD de l'ï¿½tudiant  du Matricule" + E.getMatricule());
 			}
 		connect.close();
 	 }
+
+	 DBConnection BD= new DBConnection();
+	 Connection connect=BD.getConn();
+	 
+	 Statement stmt = connect.createStatement();
+	 String sql = "INSERT into etudiant values (" + E.getMatricule() + ",'" + E.getNom() + "','" + E.getPrenom() + "','" + E.getEmail() + "'," +E.getNbLivreMensuel_Autorise() + "," +E.getNbLivreEmprunte() + "," +E.getId_universite()+")";
+	 int rs = stmt.executeUpdate(sql);
+	 
+	 if (rs == 1){
+			 System.out.println("log : ajout dans la BD rï¿½ussi de l'ï¿½tudiant  du Matricule" + E.getMatricule());
+		 }else if (rs == 0){
+			 System.out.println("log : Echec de l'ajout dans la BD de l'ï¿½tudiant  du Matricule" + E.getMatricule());
+		 }
+	 connect.close();
+  }
 
 
 	boolean Exists(String email) throws SQLException	
@@ -55,7 +70,7 @@ public class EtudiantRepository {
 		boolean rs = stmt.execute(sql);
 		
 		if (rs){
-			System.out.println("logBD--- :etudiant avec ce matricule existe déja dans la BD  " + mat);
+			System.out.println("logBD--- :etudiant avec ce matricule existe dï¿½ja dans la BD  " + mat);
 			connect.close();
 			return true;
 			}
--- src/EtudiantService.java
+++ src/EtudiantService.java
@@ -6,14 +6,14 @@ import java.util.ArrayList;
 public class EtudiantService {
 	
 	
-	boolean inscription (int matricule, String nom, String prénom, String email,String pwd, int id_universite) throws SQLException	
+	boolean inscription (int matricule, String nom, String prï¿½nom, String email,String pwd, int id_universite) throws SQLException	
 	{
 		EtudiantRepository StudRep= new EtudiantRepository();
 	    UniversiteRepository UnivRep= new UniversiteRepository();
-	    Etudiant stud = new Etudiant(matricule, nom, prénom, email,pwd,id_universite);
+	    Etudiant stud = new Etudiant(matricule, nom, prï¿½nom, email,pwd,id_universite);
 	    Universite univ=UnivRep.GetById(id_universite);
 	    
-	    System.out.println("Log: début de l'opération d'ajout de l'étudiant avec matricule "+matricule);
+	    System.out.println("Log: dï¿½but de l'opï¿½ration d'ajout de l'ï¿½tudiant avec matricule "+matricule);
 	    
 	    if(email == null || email.length() == 0)
 	    {
@@ -24,7 +24,41 @@ public class EtudiantService {
 	    {
 	        return false;
 	    }
+	    boolean inscription (int matricule, String nom, String prï¿½nom, String email,String pwd, int id_universite) throws SQLException	
+	{
+		EtudiantRepository StudRep= new EtudiantRepository();
+	    UniversiteRepository UnivRep= new UniversiteRepository();
+	    Etudiant stud = new Etudiant(matricule, nom, prï¿½nom, email,pwd,id_universite);
+	    Universite univ=UnivRep.GetById(id_universite);
 	    
+	    System.out.println("Log: dï¿½but de l'opï¿½ration d'ajout de l'ï¿½tudiant avec matricule "+matricule);
+	    
+	    if(email == null || email.length() == 0)
+	    {
+	    	return false;
+	    }
+	    
+	    if (StudRep.Exists(matricule))
+	    {
+	        return false;
+	    }boolean inscription (int matricule, String nom, String prï¿½nom, String email,String pwd, int id_universite) throws SQLException	
+		{
+			EtudiantRepository StudRep= new EtudiantRepository();
+			UniversiteRepository UnivRep= new UniversiteRepository();
+			Etudiant stud = new Etudiant(matricule, nom, prï¿½nom, email,pwd,id_universite);
+			Universite univ=UnivRep.GetById(id_universite);
+			
+			System.out.println("Log: dï¿½but de l'opï¿½ration d'ajout de l'ï¿½tudiant avec matricule "+matricule);
+			
+			if(email == null || email.length() == 0)
+			{
+				return false;
+			}
+			
+			if (StudRep.Exists(matricule))
+			{
+				return false;
+			}
 		if (StudRep.Exists(email))
 	    {
 	        return false;
@@ -42,7 +76,7 @@ public class EtudiantService {
 	     }                           
 	     
 		 StudRep.add(stud);
-		 System.out.println("Log: Fin de l'opération d'ajout de l'étudiant avec matricule "+matricule);
+		 System.out.println("Log: Fin de l'opï¿½ration d'ajout de l'ï¿½tudiant avec matricule "+matricule);
 		 return true;
 	    
 		
