5f5d2536b3c4919e75bfb1303a6d18d14161887d
Your Name <ouissalbraik02@gmail.com>
2022-12-07 12:50:35 +0000

    d’afficher tous les messages sur un fichier

--- src/Universite.java
+++ src/Universite.java
@@ -26,7 +26,23 @@ public class affichag  {
    
 	public void setPack(TypePackage pack) {
 		this.pack = pack;
-	}
+	} public class affichag  {
+
+		private int i_univ; //identifiant unique de l'universit�
+		private String nom;
+		private TypePackage pack;
+		
+		   public affichag(int i_univ, String nom, TypePackage pack) {
+		   super();
+		   this.i_univ = i_univ;
+		   this.nom = nom;
+		   this.pack = pack;
+	   }
+	   
+		public void setPack(TypePackage pack) {
+			this.pack = pack;
+		} 
+		
 	 
  
 
